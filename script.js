f24d5458e185cfcf517022b1d24ee8b21f0c0f00
Brandon Ortega <114270169+brando161@users.noreply.github.com>
2024-01-21 18:18:35 -0500

    Create function to get photos

--- script.js
+++ script.js
@@ -2,3 +2,17 @@
 const count = 10;
 const apiKey = 'API_KEY_PLACEHOLDER';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Get photos from API
+async function getPhotos() {
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        console.log(data);
+    } catch(error) {
+        console.log(error)
+    }
+}
+
+//On Load
+getPhotos();
